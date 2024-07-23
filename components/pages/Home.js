a847950efc39b75c5345cea27ff574dbadaf257a
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-07-23 17:23:58 -0400

    Add files via upload

---
+++ components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import Main from '../Main';
+import Cards from '../Cards';
+
+
+function Home() {
+  return (
+    <>
+      <Main />
+      
+      
+      
+    </>
+  );
+}
+
+export default Home;
