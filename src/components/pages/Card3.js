56226e4756e962b091bb78ac594233aa5ab99e0d
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-07-29 15:04:37 -0400

    Create Card3.js

---
+++ src/components/pages/Card3.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Card1.css'; 
+
+function Card3() {
+  return (
+    <div className="pageContainer">
+      <img
+        className="headerImage"
+        src='/images/IMG_7420.jpg'
+        alt=''
+      />
+      <div className="pageContent">
+        <h1 className="title">Allow Yourself to Grow</h1>
+        <span className="date">2 hours ago</span>
+        <p className="description">
+         
+        It is always the situations in life where we break out from our comfort zone that allow us to grow. When I initially moved out for university, I was terrified of what I’d have to face on my own, but I told myself that the change would be good for me and that I’d learn how to be independent. It wasn’t easy navigating my first month on my own—figuring out where to get lunch, what building my math class was in, and whether the friends I was making were genuine. Recently, I just finished my fourth semester of university, and all I can say is how much I’ve grown since I left.
+This experience doesn’t just apply to moving out; it applies to many scenarios you’ll encounter in life. Thinking about the friends I’ve made in the last two years, I’m grateful for every single one of them, but I’ve never shut myself away from making new connections. It’s often the most unexpected friendships that end up being the best. Let's say you get a really amazing job opportunity across the country, requiring you to leave your family and friends behind and start fresh. At first, it may seem daunting, but consider this: new city, new people, different atmosphere, and all the new experiences you’ll have. 
+Of course, all of these things won’t come instantly—you’ll have to put yourself out there. If you always think small and stay in your bubble, you’ll remain stagnant with no room for growth. One day, when you’re older, you may look back and regret the choices you didn’t make because you were scared of stepping out of your shell. Embrace life to the fullest now and evolve!
+
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card3;
