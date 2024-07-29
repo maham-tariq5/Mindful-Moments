83cfe087adfbafecff89630f45ce5efb78f823b0
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-07-29 14:27:59 -0400

    Create Card2.js

---
+++ src/components/pages/Card2.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Card1.css'; 
+
+function Card2() {
+  return (
+    <div className="pageContainer">
+      <img
+        className="headerImage"
+        src='/images/Maham.jpg'
+        alt=''
+      />
+      <div className="pageContent">
+        <h1 className="title">Patience is a Virtue</h1>
+        <span className="date">2 hours ago</span>
+        <p className="description">
+         
+        How many times have you felt like a failure in a depressive slump looking for the light but stuck in the darkness? 
+        I’ve felt like this countless times in my life wondering “am I not working hard enough”, 
+        “are others smarter then me” “do I even deserve it” etc. 
+        Looking for the answers to those questions always kept me overthinking wondering what side of the road I was on. 
+        I turned off all the distractions around on me and locked all my negative thoughts away and channeled positivity inside my brain. 
+        When you tell yourself countless times “I’m not capable of this” “what’s even the point no one will care” you will never succeed and the darkness will win. 
+        By shifting my outlook to a mindset of "I don't know how, but I will," I transformed my life. 
+        Although it may sound clichéd, maintaining a positive mindset, especially when facing adversity, is essential. 
+        Rejection is merely redirection toward what is truly meant for us. 
+        Embracing patience in the process is crucial, for patience is indeed a virtue.
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card2;
