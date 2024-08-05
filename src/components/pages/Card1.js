2f70780161bb800546f139bb60af9d98dd06c8ed
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-08-05 17:48:05 -0400

    Update Card1.js

--- src/components/pages/Card1.js
+++ src/components/pages/Card1.js
@@ -13,7 +13,7 @@ function Card1() {
         <h1 className="title">Melodrama By Lorde Concept Short Story</h1>
         <span className="date">2 hours ago</span>
         <div className="description">
-          <h2 className="chapterTitle">Chapter 1 Green Light</h2>
+          <h3 className="chapterTitle">Chapter 1 Green Light</h3>
           <p>
             We broke up a month ago, and I'm feeling all these emotions now. 
             When you said we were over for good this time, 
@@ -36,6 +36,153 @@ function Card1() {
             I just need some sign from the universe to move on from this experience. 
             I'm waiting for it, that green light—I want it.
           </p>
+
+          <h3 className="chapterTitle">Chapter 2: Sober</h3>
+          <p>
+            My friend Albert told me that I need to move on from the breakup. 
+            He suggested that we throw a house party to forget about my heartbreak and live a little. 
+            We contacted everyone we knew, and they all agreed. 
+            I really needed this party to distract myself from all the emotions I was feeling. 
+            It was now 9:30 pm on a Saturday evening, and the hot summer breeze was blowing. 
+            People started arriving, and the DJ played a bunch of dance music to set the vibe. 
+            I was drinking with my mates but didn’t want to get too drunk, as the night was still young.
+          </p>
+          <p>
+            There was one guy I saw from across the room. 
+            I couldn’t seem to look away and was mesmerized by his pearly white smile. 
+            We started talking and had a few shots together. 
+            He made me laugh for the first time in a month, and we danced together a bit. 
+            It felt good to let loose. 
+            My only concern was: what would we do when we’re sober?
+          </p>
+
+          <h3 className="chapterTitle">Chapter 3: Homemade Dynamite</h3>
+          <p>
+            The night went on as I danced to the beat of the music. I had been holding this guy's hand throughout the night. 
+            I was having the time of my life and felt happier than ever for once. 
+            Everybody was happy, smiling, and a few were puking because they had a little too much to drink. 
+            It started getting dark outside, so I had to turn on more lights around the house. 
+            The vibe of the party in the backyard was surreal: there were bright blue lights, a blue-tinted pool, 
+            and people dancing with their significant others. 
+            Our friends, our drinks, we felt inspired, blowing off steam with homemade dynamite.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 4: The Louvre</h3>
+          <p>
+            It was getting later and later as the party went on. I felt a strange instinct coming into my head that I couldn’t shake off. 
+            I started thinking about my ex again and how this new guy is the complete opposite of him, and actually likes the beach. 
+            Am I just wasting his time to fill an empty void of mine? Does that make me selfish? 
+            I feel like I am, and that I’m just using him for my own enjoyment. 
+            My empathetic side started kicking in, and I wouldn’t want to be treated this way if I were him. 
+            What if I’m just chasing this euphoric feeling that this guy is giving me? 
+            In the end, all I can think about is how we’re the greatest and they’ll hang us in the Louvre.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 5: Liability</h3>
+          <p>
+            I had a sudden rush of sadness that hit me, and I felt like crying. 
+            I went to my bedroom to get my thoughts back in check. 
+            I started having flashbacks of the night I was broken up with. 
+            I was in the taxi, crying as I tried to process the news I had been hit with. 
+            Apparently, I’m a storm, and he made the mistake of dancing in it. 
+            That night, I realized that no one can love me the way I want them to. 
+            I need to be the best version of myself before I give in to someone else. 
+            I came into this world alone, and I’m going to leave it alone, so I might as well enjoy my own company. 
+            I haven’t opened up to anyone about these feelings because I feel like a burden. 
+            I think I’m a little too much for people; I’m a liability.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 6: Hard Feelings</h3>
+          <p>
+            I wasn’t ready to go back downstairs and enjoy the party just yet. I wanted to stay in my dark room a little longer. 
+            I lit all the candles and set up the flowers I had bought for my room. 
+            I care for myself the way I used to care about him. I know it’s a hard pill to swallow, but we were bound to end. 
+            Our love had turned into a ghost as we sat in his car. I always felt a rush whenever I was with him, 
+            especially when we drifted while buying groceries. He was my lover, but I had outgrown him—the whole world knew except for me. 
+            These are what they call hard feelings.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 7: Loveless</h3>
+          <p>
+            I headed back downstairs as the party was still going on. My emotions were back in check, and the guy I was with all night had already left the party. 
+            All of my friends were still there, so we decided to end the night on a high note. 
+            Albert found an old tape lying around and asked me what it was. I answered that I was unsure. 
+            We gave the DJ the tape and told him to play it; it had a little note on it that said “this is my favorite tape,” so we assumed it would be upbeat. 
+            A hip-hop beat started playing, and it was very groovy to dance to. We all chanted, “L-O-V-E-L-E-S-S generation.”
+          </p>
+
+          <h3 className="chapterTitle">Chapter 8: Sober II (Melodrama)</h3>
+          <p>
+            The party was finally over, and it was about 3 am. I’m glad I took Albert’s advice and threw the party. 
+            I felt like a whole different person today, and I hope I continue to feel this way. 
+            Everyone was gone, and I was left alone to clean up the mess that had been created. 
+            It’s crazy how fast the evening and night pass while you’re having fun. 
+            I was cleaning up the champagne glasses while staring at the backyard and seeing all the trash. 
+            I guess this is what they call melodrama.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 9: Writer in the Dark</h3>
+          <p>
+            I open up my journal and write down how I feel every morning so I can slowly overcome these emotions. 
+            I remember him breaking the news to me and leaving me in pieces as he calmly walked out. 
+            I bet he has moved on and found someone better, someone who is good enough for him. 
+            Even though our love died, I will still love him until his breathing stops. 
+            I’ll find a way to be without him, even in my darkest hours. Breakups can be a hard thing for a person to go through. 
+            For me, it was a completely new concept, as he was my first significant other. 
+            It’s sad knowing that the person you spend every waking moment with can leave your life in the blink of an eye. 
+            At the end of the day, that’s life. You’re going to have many people walking out on you; all you can do is move on and find your destiny in this cruel world. 
+            One thing I’m always going to remember is that I’m a writer in the dark.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 10: Supercut</h3>
+          <p>
+            In my head, all I can replay are the good memories I have left of him: all the magic we shared and the love we had and lost. 
+            I remember us being in his car, trying to talk about our relationship, but the radio was too loud. 
+            All these clips replay in my head over and over again, and I end up ignoring what reality actually is. 
+            I remember traveling the world and how being so far apart from him was hard; long distance isn’t easy at all. 
+            I was performing on stages and meeting new people while he was in my hometown taking pictures. 
+            In my head, I did everything right, or at least I thought I did, which I clearly didn’t from his perspective. 
+            In the end, it’s just a supercut of us.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 11: Liability Reprise</h3>
+          <p>
+            I think it was the party that made me feel like a liability, to be honest. 
+            Maybe the point of the party was for me to get sad and cry. 
+            I feel like I’m not the problem anymore; it’s just the feeling that the party gave me that made me think I was. 
+            I guess what I thought I was is not what I actually am. 
+            I kept playing the villain card and made myself believe that I was the bad guy, but in reality, no one is. 
+            I guess I’m not actually a liability.
+          </p>
+
+          <h3 className="chapterTitle">Chapter 12: Perfect Places</h3>
+          <p>
+            Drinking, partying, and doing drugs are common activities among teenagers and young adults. 
+            When you're a child, you think the world is like a fairy tale and everything is perfect, 
+            but as you get older, you start to see the bigger picture. 
+            I am not the same person I was when I was 16 years old. 
+            Back then, I was filled with angst, felt like a gladiator, and thought I was unstoppable. 
+            As I grew older, I soon realized that I was just a little fish in this big ocean. 
+            I was hit with all these new emotions, responsibilities, and more.
+          </p>
+          <p>
+            As young adults, we are still navigating our experiences and finding ourselves. 
+            Some of us feel like we have no purpose in this world and use party culture to distract our minds from real-world problems. 
+            I am definitely someone who has done this, and I'm not very proud of it. 
+            I'm not saying you shouldn’t live out your youth and party, but don’t make it the center of your life. 
+            You should have goals, work hard, and have big plans, but don’t ignore those important things for parties.
+          </p>
+          <p>
+            You can go anywhere in the world to distract your brain from issues in your life, but in reality, there is no perfect place that exists. 
+            After the hangover hits or the drugs wear off, you're back in the same shitty state you were in before. 
+            All those nights spent off our faces, trying to find these perfect places—what the fuck are perfect places, anyway?
+          </p>
+
+
+
+
+
+
         </div>
       </div>
     </div>
