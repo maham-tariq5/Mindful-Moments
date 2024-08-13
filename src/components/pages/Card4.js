566b568591c5743cd50b077c5883a23ae1c4f790
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-08-13 17:31:10 -0400

    Update Card4.js

--- src/components/pages/Card4.js
+++ src/components/pages/Card4.js
@@ -1,1 +1,36 @@
+import React from 'react';
+import './Card1.css'; // Import the updated CSS file
+
+function Card4() {
+  return (
+    <div className="pageContainer">
+      <img
+        className="headerImage"
+        src='/images/Travel.jpg'
+        alt=''
+      />
+      <div className="pageContent">
+        <h1 className="title">Keep The Pure Hearts Close to You</h1>
+        
+        <p className="description">
+          The world can be a terrible and evil place, filled with many messed-up people. It’s hard to know who is genuine these days. We live in a society where we can't enjoy our rewards without someone belittling us and praying for our downfall. If we feed into this negativity, it corrupts our moods and makes us think bad thoughts towards those people. But if we do nothing, their behavior continues without punishment.
+        </p>
+        
+        <p className="description">
+          All my life, I've been told, "Do good things, and good things will come back to you." To a certain extent, I believe this is true, and maybe that’s why my life is good in many aspects. However, there is a limit to the goodness you can provide in the world before people start taking advantage of your kindness.
+        </p>
+        
+        <p className="description">
+          You are a product of your environment. If you choose to be a bitter, selfish, and jealous person, and surround yourself with people who fall into that category, you will never feel bad about resenting others for no reason. The difference between a good person doing something bad and a bad person doing something bad is that a good person can distinguish that their feelings or actions were wrong. If you're around people with bitter hearts, you will inevitably become just like them. Surround yourself with individuals who wish the best for you—those who cheer you on, who smile when you tell them about accomplishing one of your goals.
+        </p>
+        
+        <p className="description">
+          Growing up, I was mistreated by so many people in elementary school. I never had a long-lasting friendship with anyone at that stage of my life. As I grew older, I realized that I was a pure-hearted girl surrounded by awful people, and God wasn't rocking with that, so He removed those people from my life. Now, at 20 years old, the people I spend my time with are the most down-to-earth, caring, and considerate human beings. I truly believe I have an invisible string connecting me to them.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card4;
 
