c15ceecdbda8828c90e55b1484ce6d72bbc59077
Maham Tariq <156241209+maham-tariq5@users.noreply.github.com>
2024-08-19 22:00:34 -0400

    Create Card5.js

---
+++ src/components/pages/Card5.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Card1.css'; // Import the updated CSS file
+
+function Card5() {
+  return (
+    <div className="pageContainer">
+      <img
+        className="headerImage"
+        src='/images/Travel.jpg'
+        alt=''
+      />
+      <div className="pageContent">
+        <h1 className="title">A Sibling Bond is Forever</h1>
+        
+        <p className="description">
+        The day was Thursday, February 5th, 2004, and the time was 10:00am. My mother had just given birth to her second child, and her first daughter. My five year old brother waited in front of the entrance door of our house waiting for my mother to come home from the hospital. She eventually came home and said to my brother the following words, “Daniyal, do you want to hold your new baby sister, Maham?” His face lit up with excitement as he held me with joy in his arms. 
+        </p>
+        
+        <p className="description">
+        Just like any sibling parallel relationship, my brother and I fought a lot when we were kids, particularly over the most unnecessary things like who got to play on the Xbox, or me finishing the last cupcake he claimed. We would be upset with each other for a few minutes, and shortly after we would apologize and become best friends again. He was the older brother I never asked for, but his presence in my life quickly became a blessing in disguise. I know I can always rely on him for realistic advice, being a support system and listening to my issues, and advocating for me in disagreements with my parents. 
+        </p>
+        
+        <p className="description">
+        Sometimes, all a younger sibling wants is someone to listen to their issues and offer comfort. Many of us struggle with how we perceive ourselves in the mirror every day. Growing up, I always struggled with my appearance, just like many other women of color, and I often felt like I never matched the white beauty standard. When I was eight years old, a girl I considered my best friend told me that all the girls had made a list of the ugliest girls in the grade. I was near the top of that list. Somehow, I held back my tears, even though I’m a very emotional person. I went home that day, went straight to my room, and cried into my pillowcase. My brother came in, noticed right away that I was sobbing, and asked what had happened. I told him the story, and he hugged me and said, “Maham, you are one of the prettiest girls I know and the kindest person. Emily is just jealous and making things up—don’t believe a word she says.” In just a few seconds, the tears stopped, and I realized that one person’s opinion of me was irrelevant. My brother was only fourteen years old when he said those words, which was surprising because most boys at that age aren’t that mature. But he was different—he was my older brother, and he had a little sister to look out for. To this day, I still feel insecure about my appearance sometimes, but I always hear my brother’s voice in my head, and it makes me feel much more confident.
+        </p>
+        
+        <p className="description">
+        In certain situations, disagreements with our parents are inevitable, and sometimes our older siblings step in to back us up when things seem unfair. Growing up, I always heard people constantly praising my mom for her cooking, and I’ve always felt this burden of living up to that same standard. It doesn't help that my mother often reminds me, as if her reputation will be ruined if I can’t cook biryani like she does.
+After a long day, my brother decided to cook dinner for us. When my mother came downstairs and saw me sitting while he was cooking, the first thing she said was, "Why is he cooking? Girls are supposed to be cooking." The comment was rooted in the environment she grew up in, where cooking was always seen as a woman’s job. My brother responded, "So when I cook, I’m praised, but when Maham does, it’s expected of her because a woman belongs in the kitchen, right?" In that moment, hearing my brother defend me, I felt an overwhelming sense of relief and gratitude. It was comforting to know that even when I feel the weight of these expectations, my brother sees the unfairness and stands by me.
+        </p>
+
+        <p className= "description" >
+
+        Everyday I wake up I’m thankful to have my brother as an older sibling in my life. I don’t know what I did to deserve a person in my life who saves me from making mistakes about my future, listens to my issues, and stays on my side during arguments with our parents. In some cases, not everyone is close with their siblings and I personally think people would benefit from it. All I know is that my brother would take a bullet for me, and if he died in this moment, as I write this, I would surely lose myself.
+        </p>
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Card5;
